292295f0712096d306f0c5ac2a03e99c0139cb8e
duongbill <billduongg@gmail.com>
2024-11-17 17:32:45 +0700

    Resolve merge conflict in test.Designer.cs

--- QL_sald/test.Designer.cs
+++ QL_sald/test.Designer.cs
@@ -33,18 +33,20 @@
             // 
             // cài_đặt1
             // 
-            this.cài_đặt1.Location = new System.Drawing.Point(450, 179);
+            this.cài_đặt1.Location = new System.Drawing.Point(57, 56);
+            this.cài_đặt1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.cài_đặt1.Name = "cài_đặt1";
-            this.cài_đặt1.Size = new System.Drawing.Size(1121, 745);
+            this.cài_đặt1.Size = new System.Drawing.Size(841, 605);
             this.cài_đặt1.TabIndex = 0;
+     
             // 
             // test
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1181, 753);
+            this.ClientSize = new System.Drawing.Size(886, 612);
             this.Controls.Add(this.cài_đặt1);
-            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "test";
             this.Text = "test";
             this.ResumeLayout(false);
